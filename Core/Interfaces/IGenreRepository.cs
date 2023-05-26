63e4e62746f76d4d911a43a5ea291ebf7b0e73e3
ahmadrashed1999 <ahmadrashed240@gmail.com>
2023-05-26 19:06:33 +0300

    add another controller

---
+++ Core/Interfaces/IGenreRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections.Generic;
+using OAWEBAPI.Core.Entities;
+
+namespace OAWEBAPI.Core.Interfaces
+{
+    public interface IGenreRepository
+    {
+        Genre GetById(int id);
+        IEnumerable<Genre> GetAll();
+        void Add(Genre genre);
+        void Update(Genre genre);
+        void Delete(Genre genre);
+
+        IEnumerable<Genre> SearchGenres(string keyword);
+        IEnumerable<Genre> GetGenresByCriteria(string criteria);
+        IEnumerable<Genre> GetGenresWithBooks();
+        IEnumerable<Genre> GetGenresWithPagination(int page, int pageSize);
+    }
+}
---
+++ OAWEBAPI/Controllers/GenresController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using OAWEBAPI.Core.Entities;
+using OAWEBAPI.Core.Interfaces;
+namespace OAWEBAPI.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GenresController : ControllerBase
+    {
+        private readonly IGenreRepository _genreRepository;
+
+        public GenresController(IGenreRepository genreRepository)
+        {
+            _genreRepository = genreRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllGenres()
+        {
+            var genres = _genreRepository.GetAll();
+            return Ok(genres);
+        }
+
+        // Additional actions for handling genre-related operations
+    }
+}
